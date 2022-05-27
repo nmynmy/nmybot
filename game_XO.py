9cf7dcfa969f9820996a327e8f1e30748e2606a7
student <polic.21@mail.ru>
2022-05-27 12:45:00 +0300

    tictactoe

--- game_XO.py
+++ game_XO.py
@@ -11,9 +11,10 @@ def generate_board(call):
     print(matrix)
     convert_matrix = [('❌' if i == 1 else '⭕') if i != 2 else '⬜' for i in matrix]
     users[call.from_user.id]['convert_matrix'] = convert_matrix
-    board =dict(zip(range(9),convert_matrix))
+    board = dict(zip(range(9),convert_matrix))
     board_keys = [Button(button_data={'text': board[key], 'callback_data': str(key)}).button for key in board]
     return board_keys
+
 def check_games(call):
     users[call.from_user.id]['search_game'] = True
     buffer_user = []
@@ -30,17 +31,17 @@ def check_win(call):
     for each in win_coord:
         if board[each[0]] == board[each[1]] == board[each[2]] and users[call.from_user.id]['step'] >= 3:
             print(board[each[0]],board[each[1]],board[each[2]])
-            return "Победа"
+            return "❌❌❌ Ты победил! ❌❌❌"
     if users[call.from_user.id]['step'] == 9:
-        return "Ничья"
+        return "❌❌❌ Ничья ⭕⭕⭕"
     else:
         return False
 
 def bot_recursiv(call):
     if users[call.from_user.id]['side'] == 1:
-        cp_side=0
+        cp_side = 0
     else:
-        cp_side=1
+        cp_side = 1
     board = users[call.from_user.id]['matrix']
     for each in win_coord:
         if board[each[0]] == board[each[1]] and board[each[2]] == 2:
--- main_XO.py
+++ main_XO.py
@@ -2,37 +2,42 @@ import telebot
 from game_XO import *
 from telebot import types
 import random
+
 TOKEN = "5424381811:AAGtPI_Pni16IYb_MPJrTam2kw4E0a4LROg"
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start']) #Начальный выбор
 def start_menu(message):
-    button_list =["Сыграть с ботом","Найти игру"]
+    button_list = ["Сыграть с ботом","Найти игру"]
     bot.send_message(message.from_user.id, "выбери категорию", reply_markup=generate_menu(button_list))
     bot.delete_message(message.chat.id, message.message_id)
-    users.update({message.from_user.id: {'matrix': [2] * 9, 'side': 1, 'step':0,'oppo': None}})
+    users.update({message.from_user.id: {'matrix': [2] * 9, 'side': 1, 'step': 0 ,'oppo': None}})
 
 
     @bot.callback_query_handler(func=lambda call: True)
     def call_back(call):
-        if call.data == "Найти игру":
+        if call.data == "Найти игру": #Мультиплеер
             check_games(call)
             if users[call.from_user.id]['oppo'] is not None:
 
                 bot.send_message(call.message.chat.id, "играй че ты а ?", reply_markup=generate_menu(generate_board(call)))
-        elif call.data == "Сыграть с ботом":
+
+        elif call.data == "Сыграть с ботом":  #Синглплеер
+            bot.send_message(call.message.chat.id, "Ты играешь за ❌")
+            bot.send_message(call.message.chat.id, "Бот играет за ⭕")
             bot.send_message(call.message.chat.id, "играй че ты а ?", reply_markup=generate_menu(generate_board(call)))
+
         elif call.data in [str(i) for i in range(9)]:
             coordinate = int(call.data)
             new_matrix = users[call.from_user.id]['matrix']
             new_matrix[coordinate] = users[call.from_user.id]['side']
             users[call.from_user.id]['matrix'] = new_matrix
             print(users[call.from_user.id]['matrix'])
-            users[call.from_user.id]['step']+=1
+            users[call.from_user.id]['step'] += 1
             bot_recursiv(call)
             check = check_win(call)
-            if check == False:
+            if not check:
                 bot.send_message(call.message.chat.id, "играй че ты а ?", reply_markup=generate_menu(generate_board(call)))
             else:
                 bot.send_message(call.message.chat.id, check)
